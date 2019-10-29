f2092648b3ccebbc98e12bb588a15fecbe78e1c2
Ridvan Cekmez <ridvanceko@gmail.com>
2019-10-28 19:09:44 -0700

    client view side created (html)

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+
+var PORT = process.env.PORT || 8080;
+
+
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(bodyParser.json({ type: 'application/*+json' }))
+
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+
+app.listen(PORT, function () {
+  console.log("App connected PORT:" + PORT);
+});
+
