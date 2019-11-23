0adffe89d2446a38230a95dbde8b060e404f5bc2
Ridvan Cekmez <ridvanceko@gmail.com>
2019-11-23 14:10:59 -0700

    routes added

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
   app.get("/survey", function(request, response){
     response.sendFile(path.join(__dirname + "/../view/survey.html"));
   });
-    app.use(function(request, response){
+    app.use("/", function(request, response){
       response.sendFile(path.join(__dirname + "/../view/home.html"));
     });
   
